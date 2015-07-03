6c038286e3d39c39802d5262ec9271fb0191fea7
AlexG31 <AresEyE@163.com>
2015-07-03 13:03:17 +0800

    nchage

--- Dijkstra/Dijkstra.cpp
+++ Dijkstra/Dijkstra.cpp
@@ -13,17 +13,31 @@ typedef int dijk_type;//cost data type
 public:
 		Dijkstra_gpf(int Nedge,int Nnode):edge_num(Nedge),node_num(Nnode){
 			//Create Edge Array
-			edge=new int*[Nnode];
+			edge=new dijk_type*[Nnode];
 			for(int i=0;i<Nnode;i++){
-				edge[i]=new int[Nnode];
+				edge[i]=new dijk_type[Nnode];
+			}
+			//init:
+			for(int i=0;i<Nnode;i++){
+				for(int j=0;j<Nnode;j++)edge[i][j]=-1;
 			}
 
 		}
 		 int edge_weight_input(){
 		 	//ToDo..
+			
+			while(scanf("%d%d",&a,&b)==2){
+				if(a==-1||b==-1){break;}
+				dijk_type val;
+				cin>>val;
+				edge[a][b]=val;
+				edge[b][a]=val;
+			}
 		 }
 
 		 dijk_type dijkstra_n2(int ind1,int ind2){
+			//O(n2) algorithm
+
 		 	return ind1+ind2;
 		 }
 
